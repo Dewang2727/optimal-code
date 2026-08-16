5e7b7332390f0f36f139e3409bf6cc1355b9f387
Dewang <128068399+Dewang2727@users.noreply.github.com>
2026-08-16 19:18:12 +0530

    Create cash.c

---
+++ cash.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int cents;
+
+    do
+    {
+        printf("Change owed: ");
+        scanf("%d", &cents);
+    }
+    while (cents < 0);
+
+    int coins = cents / 25;
+    cents %= 25;
+
+    coins += cents / 10;
+    cents %= 10;
+
+    coins += cents / 5;
+    cents %= 5;
+
+    coins += cents;
+
+    printf("%d\n", coins);
+}
